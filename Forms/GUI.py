90cbfa092de12852bfcdd7777842a25690a8ac50
gazmack86 <39755491+gazmack86@users.noreply.github.com>
2018-05-30 16:18:04 +1000

    Forms bells and wistles

---
+++ Forms/GUI.py
@@ -0,0 +1,9 @@
+import kivy
+kivy.require('1.8.0') # replace with your current kivy version !
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.config import Config
+from kivy.core.window import Window
+
+Window.size = (400,130)
